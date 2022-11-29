0228ccf9f29d3038b99a24ac4645c8a10666e8fd
Travis Fischer <fisch0920@gmail.com>
2022-11-28 18:59:19 -0600

    feat: add rate limiting

--- src/lexica-api.ts
+++ src/lexica-api.ts
@@ -1,30 +1,42 @@
 import got from 'got'
+import pThrottle from 'p-throttle'
 
 import * as types from './types'
 
+/**
+ * Rate-limit HTTP requests to Lexica.
+ */
+const throttle = pThrottle({
+  limit: 20,
+  interval: 60000
+})
+
 export class LexicaAPI {
   protected _apiBaseUrl: string
+  protected _get: (url: string, query: string) => Promise<types.SearchResults>
 
   constructor({
     apiBaseUrl = ' https://lexica.art/api/v1'
   }: { apiBaseUrl?: string } = {}) {
     this._apiBaseUrl = apiBaseUrl
+
+    this._get = throttle(this._getImpl)
   }
 
   async search(query: string): Promise<types.SearchResults> {
     const url = `${this._apiBaseUrl}/search`
-    return got<types.SearchResults>(url, {
-      searchParams: {
-        q: query
-      }
-    }).json()
+    return this._get(url, query)
   }
 
   async reverseImageSearch(imageUrl: string): Promise<types.SearchResults> {
     const url = `${this._apiBaseUrl}/search`
+    return this._get(url, imageUrl)
+  }
+
+  async _getImpl(url: string, query: string): Promise<types.SearchResults> {
     return got<types.SearchResults>(url, {
       searchParams: {
-        q: imageUrl
+        q: query
       }
     }).json()
   }
